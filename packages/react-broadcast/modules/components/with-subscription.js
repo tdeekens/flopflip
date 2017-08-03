574d3f95d7e9cc5f50e30df50c092424a6d7a71b
tdeekens <nerd@tdeekens.name>
2017-08-03 23:27:09 +0200

    fix(react-broadcast): to overwrite display name

--- packages/react-broadcast/modules/components/with-subscription.js
+++ packages/react-broadcast/modules/components/with-subscription.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import { wrapDisplayName } from 'recompose';
 import Subscriber from './subscriber';
 import { FLAGS_CHANNEL } from './configure';
 
 const withSubscription = propKey => WrappedComponent => {
   class Subscribed extends React.Component {
+    static displayName = wrapDisplayName(WrappedComponent, 'withSubscription');
     render() {
       return (
         <Subscriber channel={FLAGS_CHANNEL}>
-          {data => {
-            return (
-              <WrappedComponent {...{ [propKey]: data }} {...this.props} />
-            );
-          }}
+          {data =>
+            <WrappedComponent {...{ [propKey]: data }} {...this.props} />}
         </Subscriber>
       );
     }
