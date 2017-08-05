ec7e1fea2989c0058a38e904df3a229bc5f40a9c
tdeekens <nerd@tdeekens.name>
2017-08-05 14:59:15 +0200

    feat(react-{redux, broadcast}): to integrate with FlagSubscription

--- packages/react-broadcast/modules/components/configure.js
+++ packages/react-broadcast/modules/components/configure.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { initialize, listen } from '@flopflip/launchdarkly-wrapper';
+import { FlagsSubscription } from '@flopflip/react';
 import Broadcast from './broadcast';
 
 export const FLAGS_CHANNEL = '@flopflip';
@@ -9,32 +9,24 @@ export default class Configure extends React.Component {
   static displayName = 'ConfigureFlopflip';
 
   static propTypes = {
-    children: PropTypes.node.isRequired,
+    children: PropTypes.node,
     clientSideId: PropTypes.string.isRequired,
+    shouldInitialize: PropTypes.func,
     user: PropTypes.shape({
       key: PropTypes.string,
     }),
   };
 
   static defaultProps = {
+    children: null,
     user: {},
+    shouldInitialize: () => true,
   };
 
   state = {
     flags: {},
     status: { isReady: false },
   };
-
-  componentDidMount() {
-    listen({
-      client: initialize({
-        clientSideId: this.props.clientSideId,
-        user: this.props.user,
-      }),
-      updateFlags: this.handleUpdateFlags,
-      updateStatus: this.handleUpdateStatus,
-    });
-  }
 
   handleUpdateFlags = flags => {
     this.setState(prevState => ({
@@ -51,9 +43,19 @@ export default class Configure extends React.Component {
 
   render() {
     return (
-      <Broadcast channel={FLAGS_CHANNEL} value={this.state.flags}>
-        {this.props.children ? React.Children.only(this.props.children) : null}
-      </Broadcast>
+      <FlagsSubscription
+        clientSideId={this.props.clientSideId}
+        user={this.props.user}
+        shouldInitialize={this.props.shouldInitialize}
+        onUpdateStatus={this.handleUpdateStatus}
+        onUpdateFlags={this.handleUpdateFlags}
+      >
+        <Broadcast channel={FLAGS_CHANNEL} value={this.state.flags}>
+          {this.props.children
+            ? React.Children.only(this.props.children)
+            : null}
+        </Broadcast>
+      </FlagsSubscription>
     );
   }
 }
