3802a3c4a2e941119513fd76cb9f274c9209ff8f
tdeekens <nerd@tdeekens.name>
2017-08-05 14:55:35 +0200

    feat(react): add FlagsSubscrption component

--- packages/react/modules/components/index.js
+++ packages/react/modules/components/index.js
@@ -1,3 +1,4 @@
 export { default as FeatureToggled } from './feature-toggled';
 export { default as injectFeatureToggles } from './inject-feature-toggles';
 export { default as branchUntoggled } from './branch-untoggled';
+export { default as FlagsSubscription } from './flags-subscription';
--- packages/react/modules/index.js
+++ packages/react/modules/index.js
@@ -2,4 +2,5 @@ export {
   FeatureToggled,
   injectFeatureToggles,
   branchUntoggled,
+  FlagsSubscription,
 } from './components';
