2d0c345c7e343fee9e1f53168769fb8b63440be9
tdeekens <nerd@tdeekens.name>
2017-08-06 21:26:57 +0200

    feat(react-{redux, broadcast}): add injectFeatureToggle to exports

--- packages/react-redux/modules/components/index.js
+++ packages/react-redux/modules/components/index.js
@@ -1,4 +1,5 @@
 export { default as FeatureToggled } from './feature-toggled';
+export { default as injectFeatureToggle } from './inject-feature-toggle';
 export { default as injectFeatureToggles } from './inject-feature-toggles';
 export { default as withFeatureToggle } from './with-feature-toggle';
 export { default as ConfigureFlopFlip } from './configure';
