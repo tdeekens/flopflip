fa01b06e2765e1f55ed31452ccb16ea5ece2ed27
tdeekens <nerd@tdeekens.name>
2017-08-06 16:05:53 +0200

    refactor(react-{redux, broadcast}): with-feature-toggle to use inject-feature-toggle

--- packages/react-redux/modules/components/with-feature-toggle.js
+++ packages/react-redux/modules/components/with-feature-toggle.js
@@ -1,9 +1,6 @@
 import { compose } from 'recompose';
 import { branchUntoggled } from '@flopflip/react';
-import injectFeatureToggles from './inject-feature-toggles';
+import injectFeatureToggle from './inject-feature-toggle';
 
-export default (featureToggle, UntoggledComponent) =>
-  compose(
-    injectFeatureToggles([featureToggle]),
-    branchUntoggled(UntoggledComponent)
-  );
+export default (flagName, UntoggledComponent) =>
+  compose(injectFeatureToggle(flagName), branchUntoggled(UntoggledComponent));
