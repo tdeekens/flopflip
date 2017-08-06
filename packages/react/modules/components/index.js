c52dbff3242b349f2753f7a45a6445ed38ce9c77
tdeekens <nerd@tdeekens.name>
2017-08-06 16:20:50 +0200

    fix(react): add exports of inject-feature-toggle

--- packages/react/modules/components/index.js
+++ packages/react/modules/components/index.js
@@ -1,4 +1,5 @@
 export { default as FeatureToggled } from './feature-toggled';
+export { default as injectFeatureToggle } from './inject-feature-toggle';
 export { default as injectFeatureToggles } from './inject-feature-toggles';
 export { default as branchUntoggled } from './branch-untoggled';
 export { default as FlagsSubscription } from './flags-subscription';
