4a6f725f74a411f9715d89dc47e042e614046f73
tdeekens <tobias.deekens@commercetools.de>
2017-08-10 22:45:22 +0200

    build: add linting on ci

--- packages/react-broadcast/modules/components/feature-toggled.js
+++ packages/react-broadcast/modules/components/feature-toggled.js
@@ -1,13 +1,10 @@
 import { compose, withProps } from 'recompose';
-import isNil from 'lodash.isnil';
 import { FeatureToggled } from '@flopflip/react';
 import withSubscription from './with-subscription';
 
 export default compose(
   withSubscription('availableFeatureToggles'),
-  withProps(props => {
-    const flagValue = props.availableFeatureToggles[props.flag];
-
-    return { isFeatureEnabled: isNil(flagValue) ? false : flagValue };
-  })
+  withProps(props => ({
+    isFeatureEnabled: Boolean(props.availableFeatureToggles[props.flag]),
+  }))
 )(FeatureToggled);
