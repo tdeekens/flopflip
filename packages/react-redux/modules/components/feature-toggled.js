ced3ae545d53b8bdf4aa25a4507e75dc64464f70
tdeekens <tobias.deekens@commercetools.de>
2017-08-10 22:45:22 +0200

    build: add linting on ci

--- packages/react-redux/modules/components/feature-toggled.js
+++ packages/react-redux/modules/components/feature-toggled.js
@@ -1,12 +1,9 @@
 import { connect } from 'react-redux';
-import isNil from 'lodash.isnil';
 import { FeatureToggled } from '@flopflip/react';
 import { STATE_SLICE } from './../store';
 
-export const mapStateToProps = (state, ownProps) => {
-  const flagValue = state[STATE_SLICE].flags[ownProps.flag];
-
-  return { isFeatureEnabled: isNil(flagValue) ? false : flagValue };
-};
+export const mapStateToProps = (state, ownProps) => ({
+  isFeatureEnabled: Boolean(state[STATE_SLICE].flags[ownProps.flag]),
+});
 
 export default connect(mapStateToProps)(FeatureToggled);
