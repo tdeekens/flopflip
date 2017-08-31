979a22fa486a88ebf012a0ae69b9f5ffa693d721
tdeekens <nerd@tdeekens.name>
2017-09-01 00:29:52 +0200

    fix: to safely extract flag name and variate

--- packages/react-redux/modules/components/with-feature-toggle.js
+++ packages/react-redux/modules/components/with-feature-toggle.js
@@ -1,6 +1,24 @@
 import { compose } from 'recompose';
+import isObject from 'lodash.isobject';
 import { branchUntoggled } from '@flopflip/react';
 import injectFeatureToggle from './inject-feature-toggle';
 
-export default ({ flagName, flagVariate }, UntoggledComponent) =>
-  compose(injectFeatureToggle(flagName), branchUntoggled(UntoggledComponent));
+const safelyExtractFlagAndVariate = options => {
+  if (!isObject(options)) {
+    // "Legacy" API with just the name as parameter
+    console.warn(
+      '@flopflip/react-redux/with-feature-toggle: Please provide an object `{ flag: String, variant: ?String }`.'
+    );
+    return { name: options };
+  }
+
+  return options;
+};
+
+export default (options, UntoggledComponent) => {
+  const { flag, variate } = safelyExtractFlagAndVariate(options);
+  return compose(
+    injectFeatureToggle(flag),
+    branchUntoggled(UntoggledComponent, flag, variate)
+  );
+};
