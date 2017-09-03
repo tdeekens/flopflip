02e422c257b629463bdfd84a09a6aae8fed08b80
tdeekens <nerd@tdeekens.name>
2017-09-03 14:32:51 +0200

    feat: add support for default variate value

--- packages/react/modules/components/branch-untoggled.js
+++ packages/react/modules/components/branch-untoggled.js
@@ -4,10 +4,12 @@ import isFeatureEnabled from '../helpers/is-feature-enabled';
 const branchUntoggled = (
   UntoggledComponent,
   flagName = 'isFeatureEnabled',
-  flagVariate = true
+  flagVariate = true,
+  defaultVariateValue
 ) =>
   branch(
-    props => !isFeatureEnabled(flagName, flagVariate)(props),
+    props =>
+      !isFeatureEnabled(flagName, flagVariate, defaultVariateValue)(props),
     UntoggledComponent ? renderComponent(UntoggledComponent) : renderNothing
   );
 
