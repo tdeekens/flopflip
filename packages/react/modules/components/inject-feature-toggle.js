4e3b54851951ae323401b11561861d8f0a4e8664
tdeekens <nerd@tdeekens.name>
2017-09-05 19:45:14 +0200

    refactor(react): to omit prop containing all feature toggles

--- packages/react/modules/components/inject-feature-toggle.js
+++ packages/react/modules/components/inject-feature-toggle.js
@@ -1,12 +1,16 @@
-import { withProps } from 'recompose';
+import { compose, withProps } from 'recompose';
 import isNil from 'lodash.isnil';
 import { defaultFlagName } from '../helpers/is-feature-enabled';
+import omitProps from '@hocs/omit-props';
 
 const injectFeatureToggle = (flagName, propKey = defaultFlagName) =>
-  withProps(props => {
-    const flagValue = props.availableFeatureToggles[flagName];
+  compose(
+    withProps(props => {
+      const flagValue = props.availableFeatureToggles[flagName];
 
-    return { [propKey]: isNil(flagValue) ? false : flagValue };
-  });
+      return { [propKey]: isNil(flagValue) ? false : flagValue };
+    }),
+    omitProps('availableFeatureToggles')
+  );
 
 export default injectFeatureToggle;
