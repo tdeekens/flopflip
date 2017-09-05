cb3b7d5d7d41dcfb436bec9232faf84a5e1d4dfd
tdeekens <nerd@tdeekens.name>
2017-09-05 19:25:12 +0200

    refactor(react): to use defaultFlagName constant

--- packages/react/modules/components/inject-feature-toggle.js
+++ packages/react/modules/components/inject-feature-toggle.js
@@ -1,7 +1,8 @@
 import { withProps } from 'recompose';
 import isNil from 'lodash.isnil';
+import { defaultFlagName } from '../helpers/is-feature-enabled';
 
-const injectFeatureToggle = (flagName, propKey = 'isFeatureEnabled') =>
+const injectFeatureToggle = (flagName, propKey = defaultFlagName) =>
   withProps(props => {
     const flagValue = props.availableFeatureToggles[flagName];
 
