3aff6080532219f9a7b6dbf7018067481b27b469
tdeekens <nerd@tdeekens.name>
2017-09-05 19:45:14 +0200

    refactor(react): to omit prop containing all feature toggles

--- packages/react/modules/components/inject-feature-toggles.js
+++ packages/react/modules/components/inject-feature-toggles.js
@@ -1,5 +1,6 @@
-import { withProps } from 'recompose';
+import { compose, withProps } from 'recompose';
 import intersection from 'lodash.intersection';
+import omitProps from '@hocs/omit-props';
 
 const filterFeatureToggles = (availableFeatureToggles, flagNames) =>
   intersection(Object.keys(availableFeatureToggles), flagNames).reduce(
@@ -10,11 +11,14 @@ const filterFeatureToggles = (availableFeatureToggles, flagNames) =>
   );
 
 const injectFeatureToggles = flagNames =>
-  withProps(props => ({
-    featureToggles: filterFeatureToggles(
-      props.availableFeatureToggles,
-      flagNames
-    ),
-  }));
+  compose(
+    withProps(props => ({
+      featureToggles: filterFeatureToggles(
+        props.availableFeatureToggles,
+        flagNames
+      ),
+    })),
+    omitProps('availableFeatureToggles')
+  );
 
 export default injectFeatureToggles;
