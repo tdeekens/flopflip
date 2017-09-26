3a60bd5016023a69b4d6078200b89d5c4e8aeebb
tdeekens <nerd@tdeekens.name>
2017-09-26 20:41:40 +0200

    test: update enzyme to 3.0.0

--- config/jest.js
+++ config/jest.js
@@ -1,6 +1,9 @@
 import 'babel-polyfill';
 import * as commercetoolsEnzymeMatchers from '@commercetools/jest-enzyme-matchers';
 import * as enzymeMatchers from 'enzyme-matchers';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 
+Enzyme.configure({ adapter: new Adapter() });
 expect.extend(commercetoolsEnzymeMatchers);
 expect.extend(enzymeMatchers);
