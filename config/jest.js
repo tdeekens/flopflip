a10fc359a79b132a50de84f25f319a3ab22f6b29
tdeekens <nerd@tdeekens.name>
2017-10-17 23:37:06 +0200

    fix(test): setup with enzyme-matchers

--- config/jest.js
+++ config/jest.js
@@ -1,9 +1,8 @@
 import 'babel-polyfill';
+import 'jest-enzyme';
 import * as commercetoolsEnzymeMatchers from '@commercetools/jest-enzyme-matchers';
-import * as enzymeMatchers from 'enzyme-matchers';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.extend(commercetoolsEnzymeMatchers);
-expect.extend(enzymeMatchers);
