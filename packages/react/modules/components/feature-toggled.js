7f0b54dd938b14399622c84561942b755bc1906d
tdeekens <nerd@tdeekens.name>
2017-10-22 11:04:24 +0200

    refactor(react): FeatureToggled to accept render and components

--- packages/react/modules/components/feature-toggled.js
+++ packages/react/modules/components/feature-toggled.js
@@ -5,8 +5,10 @@ const isEmptyChildren = children => React.Children.count(children) === 0;
 
 export default class FeatureToggled extends React.PureComponent {
   static propTypes = {
-    untoggledComponent: PropTypes.node,
-    toggledComponent: PropTypes.node,
+    untoggledComponent: PropTypes.func,
+    toggledComponent: PropTypes.func,
+
+    render: PropTypes.func,
     children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]),
 
     // HoC
@@ -16,21 +18,29 @@ export default class FeatureToggled extends React.PureComponent {
   static defaultProps = {
     untoggledComponent: null,
     toggledComponent: null,
+    render: null,
     children: null,
   };
 
   render() {
     if (this.props.isFeatureEnabled) {
-      if (!isEmptyChildren(this.props.toggledComponent))
-        return React.cloneElement(this.props.toggledComponent);
-
       if (typeof this.props.children === 'function')
         return this.props.children();
 
+      if (this.props.toggledComponent)
+        return React.createElement(this.props.toggledComponent);
+
       if (this.props.children && !isEmptyChildren(this.props.children))
         return React.Children.only(this.props.children);
-    } else if (!isEmptyChildren(this.props.untoggledComponent)) {
-      return React.cloneElement(this.props.untoggledComponent);
+    }
+
+    if (typeof this.props.render === 'function')
+      return this.props.render({
+        isFeatureEnabled: this.props.isFeatureEnabled,
+      });
+
+    if (this.props.untoggledComponent) {
+      return React.createElement(this.props.untoggledComponent);
     }
 
     return null;
