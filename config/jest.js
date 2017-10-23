485433e5109f9f3221e598ae705f4867b9870601
tdeekens <nerd@tdeekens.name>
2017-10-23 19:50:06 +0200

    chore(package): update dependencies to react@16

--- config/jest.js
+++ config/jest.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import 'jest-enzyme';
 import * as commercetoolsEnzymeMatchers from '@commercetools/jest-enzyme-matchers';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.extend(commercetoolsEnzymeMatchers);
