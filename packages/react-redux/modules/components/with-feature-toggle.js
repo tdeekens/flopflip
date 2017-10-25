8e5a0291d711c9094f97540eead1b79dfc116890
tdeekens <tobias.deekens@commercetools.de>
2017-10-25 18:28:07 +0200

    breaking(react): remove safe extration of flag and variate args

--- packages/react-redux/modules/components/with-feature-toggle.js
+++ packages/react-redux/modules/components/with-feature-toggle.js
@@ -3,23 +3,9 @@ import isObject from 'lodash.isobject';
 import { branchUntoggled, DEFAULT_FLAG_PROP_KEY } from '@flopflip/react';
 import injectFeatureToggle from './inject-feature-toggle';
 
-const safelyExtractFlagAndVariate = options => {
-  if (!isObject(options)) {
-    // "Legacy" API with just the name as parameter
-    console.warn(
-      '@flopflip/react-redux/with-feature-toggle: Please provide an object `{ flag: String, variate: ?String }`.'
-    );
-    return { flag: options };
-  }
-
-  return options;
-};
-
-export default (options, UntoggledComponent) => WrappedComponent => {
-  const { flag, variate } = safelyExtractFlagAndVariate(options);
-  return compose(
+export default ({ flag, variate }, UntoggledComponent) => WrappedComponent =>
+  compose(
     injectFeatureToggle(flag),
     branchUntoggled(UntoggledComponent, DEFAULT_FLAG_PROP_KEY, variate),
     setDisplayName(wrapDisplayName(WrappedComponent, 'withFeatureToggle'))
   )(WrappedComponent);
-};
