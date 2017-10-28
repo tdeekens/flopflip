ef0cbc410240e353d169148227c4486e17708f51
tdeekens <nerd@tdeekens.name>
2017-10-28 08:38:47 +0200

    fix(react-broadcast): fix regression in argument name

--- packages/react-broadcast/modules/components/with-feature-toggle.js
+++ packages/react-broadcast/modules/components/with-feature-toggle.js
@@ -3,7 +3,7 @@ import isObject from 'lodash.isobject';
 import { branchUntoggled, DEFAULT_FLAG_PROP_KEY } from '@flopflip/react';
 import injectFeatureToggle from './inject-feature-toggle';
 
-export default ({ flag, variante }, UntoggledComponent) => WrappedComponent =>
+export default ({ flag, variate }, UntoggledComponent) => WrappedComponent =>
   compose(
     injectFeatureToggle(flag),
     branchUntoggled(UntoggledComponent, DEFAULT_FLAG_PROP_KEY, variate),
