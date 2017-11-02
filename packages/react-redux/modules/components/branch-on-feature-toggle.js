cc899bce725edffd032e3d472482f9541b3e8973
tdeekens <nerd@tdeekens.name>
2017-11-02 23:37:18 +0100

    fix(react-redux): to import branchOnFeatureToggle

--- packages/react-redux/modules/components/branch-on-feature-toggle.js
+++ packages/react-redux/modules/components/branch-on-feature-toggle.js
@@ -1,11 +1,11 @@
 import { compose, setDisplayName, wrapDisplayName } from 'recompose';
 import isObject from 'lodash.isobject';
-import { branchUntoggled, DEFAULT_FLAG_PROP_KEY } from '@flopflip/react';
+import { branchOnFeatureToggle, DEFAULT_FLAG_PROP_KEY } from '@flopflip/react';
 import injectFeatureToggle from './inject-feature-toggle';
 
 export default ({ flag, variate }, UntoggledComponent) => WrappedComponent =>
   compose(
     injectFeatureToggle(flag),
-    branchUntoggled(UntoggledComponent, DEFAULT_FLAG_PROP_KEY, variate),
+    branchOnFeatureToggle(UntoggledComponent, DEFAULT_FLAG_PROP_KEY, variate),
     setDisplayName(wrapDisplayName(WrappedComponent, 'branchOnFeatureToggle'))
   )(WrappedComponent);
