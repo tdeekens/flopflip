7042f9a31e2c73c00eec6b75c80d367cd430b0e9
tdeekens <nerd@tdeekens.name>
2017-11-25 20:51:54 +0100

    feat(react-redux): add selectors for flags and a flag

--- packages/react-redux/modules/ducks/index.js
+++ packages/react-redux/modules/ducks/index.js
@@ -3,7 +3,7 @@ import { flagsReducer } from './flags';
 import { statusReducer } from './status';
 
 export { updateStatus, UPDATE_STATUS } from './status';
-export { updateFlags, UPDATE_FLAGS } from './flags';
+export { updateFlags, UPDATE_FLAGS, selectFlag, selectFlags } from './flags';
 
 export const flopflipReducer = combineReducers({
   flags: flagsReducer,
