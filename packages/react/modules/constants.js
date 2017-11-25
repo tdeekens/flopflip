fc991201eb75a457be334bdbff0fe360e111d517
tdeekens <nerd@tdeekens.name>
2017-11-25 20:33:31 +0100

    refactor(react): rename all flags contant

--- packages/react/modules/constants.js
+++ packages/react/modules/constants.js
@@ -1,3 +1,3 @@
 export const DEFAULT_FLAG_PROP_KEY = 'isFeatureEnabled';
 export const DEFAULT_FLAGS_PROP_KEY = 'featureToggles';
-export const ALL_FLAGS = '__flopflop_all_flags__';
+export const ALL_FLAGS = '@flopflip/flags';
