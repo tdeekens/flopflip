6f40383892ad324fbe79339f78b251d932301a39
tdeekens <nerd@tdeekens.name>
2017-11-29 17:41:14 +0100

    fix(react): so that injectFeatureToggles only updates for keys

--- packages/react/modules/components/inject-feature-toggles/inject-feature-toggles.js
+++ packages/react/modules/components/inject-feature-toggles/inject-feature-toggles.js
@@ -1,4 +1,4 @@
-import { compose, withProps } from 'recompose';
+import { compose, withProps, onlyUpdateForKeys } from 'recompose';
 import intersection from 'lodash.intersection';
 import { omitProps } from '../../hocs';
 import { ALL_FLAGS_PROP_KEY, DEFAULT_FLAGS_PROP_KEY } from '../../constants';
@@ -17,7 +17,8 @@ const injectFeatureToggles = (flagNames, propKey = DEFAULT_FLAGS_PROP_KEY) =>
     withProps(props => ({
       [propKey]: filterFeatureToggles(props[ALL_FLAGS_PROP_KEY], flagNames),
     })),
-    omitProps(ALL_FLAGS_PROP_KEY)
+    omitProps(ALL_FLAGS_PROP_KEY),
+    onlyUpdateForKeys(flagNames)
   );
 
 export default injectFeatureToggles;
