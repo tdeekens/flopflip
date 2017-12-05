52df9bd56eafea10d01094a58e8b8814ac55c459
tdeekens <nerd@tdeekens.name>
2017-12-05 18:50:15 +0100

    test(react-{redux, broadcast}): migrate SwitchFeature to ToggleFeature

---
+++ packages/react-broadcast/modules/components/toggle-feature/toggle-feature.js
@@ -0,0 +1,16 @@
+import { compose, withProps } from 'recompose';
+import {
+  ToggleFeature,
+  isFeatureEnabled,
+  ALL_FLAGS_PROP_KEY,
+} from '@flopflip/react';
+import withFlagSubscription from '../with-flag-subscription/';
+
+export default compose(
+  withFlagSubscription(ALL_FLAGS_PROP_KEY),
+  withProps(props => ({
+    isFeatureEnabled: isFeatureEnabled(props.flag, props.variate)(
+      props[ALL_FLAGS_PROP_KEY]
+    ),
+  }))
+)(ToggleFeature);
---
+++ packages/react-redux/modules/components/toggle-feature/toggle-feature.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import { ToggleFeature, isFeatureEnabled } from '@flopflip/react';
+import { STATE_SLICE } from './../../store';
+
+export const mapStateToProps = (state, ownProps) => ({
+  isFeatureEnabled: isFeatureEnabled(ownProps.flag, ownProps.variate)(
+    state[STATE_SLICE].flags
+  ),
+});
+
+/* istanbul ignore next */
+export default connect(mapStateToProps)(ToggleFeature);
