c3af1b09d589a2de52de942161726c42c3fa8985
tdeekens <nerd@tdeekens.name>
2017-12-05 18:50:15 +0100

    test(react-{redux, broadcast}): migrate SwitchFeature to ToggleFeature

---
+++ packages/react-redux/modules/components/toggle-feature/feature-toggled.spec.js
@@ -0,0 +1,74 @@
+import { STATE_SLICE } from './../../store';
+import { mapStateToProps } from './feature-toggled';
+
+describe('mapStateToProps', () => {
+  describe('with existing `flag` ', () => {
+    let state;
+
+    beforeEach(() => {
+      state = {
+        [STATE_SLICE]: { flags: { flag1: true } },
+      };
+    });
+
+    it('should map `isFeatureEnabled` as `true` onto `props`', () => {
+      expect(mapStateToProps(state, { flag: 'flag1' }).isFeatureEnabled).toBe(
+        true
+      );
+    });
+  });
+
+  describe('without existing `flag` ', () => {
+    let state;
+
+    beforeEach(() => {
+      state = {
+        [STATE_SLICE]: { flags: {} },
+      };
+    });
+
+    it('should map `isFeatureEnabled` as `false` onto `props`', () => {
+      expect(mapStateToProps(state, { flag: 'flag1' }).isFeatureEnabled).toBe(
+        false
+      );
+    });
+  });
+
+  describe('with matching `variate`', () => {
+    let state;
+
+    beforeEach(() => {
+      state = {
+        [STATE_SLICE]: { flags: { flag1: 'flagVariate1' } },
+      };
+    });
+
+    it('should map `isFeatureEnabled` as `true` onto `props`', () => {
+      expect(
+        mapStateToProps(state, {
+          flag: 'flag1',
+          variate: 'flagVariate1',
+        }).isFeatureEnabled
+      ).toBe(true);
+    });
+  });
+
+  describe('with non-matching `variate`', () => {
+    let state;
+
+    beforeEach(() => {
+      state = {
+        [STATE_SLICE]: { flags: { flag1: 'flagVariate1' } },
+      };
+    });
+
+    it('should map `isFeatureEnabled` as `true` onto `props`', () => {
+      expect(
+        mapStateToProps(state, {
+          flag: 'flag1',
+          variate: 'flagVariate2',
+        }).isFeatureEnabled
+      ).toBe(false);
+    });
+  });
+});
---
+++ packages/react-redux/modules/components/toggle-feature/index.js
@@ -0,0 +1,1 @@
+export default from './toggle-feature';
