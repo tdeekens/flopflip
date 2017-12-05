b5aa8ecf258503f7b52cce4e7c0cf9f9b9105dec
tdeekens <nerd@tdeekens.name>
2017-12-05 18:45:56 +0100

    refactor(react): deprecate FeatureToggled

--- packages/react/modules/components/feature-toggled/feature-toggled.spec.js
+++ packages/react/modules/components/feature-toggled/feature-toggled.spec.js
@@ -1,162 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import warning from 'warning';
 import FeatureToggled from './feature-toggled';
 
-const UntoggledComponent = () => <div>UntoggledComponent</div>;
-UntoggledComponent.displayName = 'UntoggledComponent';
-const ToggledComponent = () => <div>ToggledComponent</div>;
-ToggledComponent.displayName = 'ToggledComponent';
-const FeatureComponent = () => <div>FeatureComponent</div>;
-FeatureComponent.displayName = 'FeatureComponent';
+jest.mock('warning');
 
-describe('when feature disabled', () => {
-  describe('with untoggled component', () => {
-    let wrapper;
-
-    beforeEach(() => {
-      wrapper = shallow(
-        <FeatureToggled
-          isFeatureEnabled={false}
-          untoggledComponent={UntoggledComponent}
-        >
-          <FeatureComponent />
-        </FeatureToggled>
-      );
-    });
-
-    it('should match snapshot', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should render the `UntoggledComponent`', () => {
-      expect(wrapper).toRender(UntoggledComponent);
-    });
-
-    it('should not render the `FeatureComponent`', () => {
-      expect(wrapper).not.toRender(FeatureComponent);
-    });
+describe('rendering', () => {
+  beforeEach(() => {
+    shallow(
+      <FeatureToggled isFeatureEnabled>
+        <div />
+      </FeatureToggled>
+    );
   });
 
-  describe('without untoggled component', () => {
-    let wrapper;
-
-    beforeEach(() => {
-      wrapper = shallow(
-        <FeatureToggled isFeatureEnabled={false}>
-          <FeatureComponent />
-        </FeatureToggled>
-      );
-    });
-
-    it('should match snapshot', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should not render the `FeatureComponent`', () => {
-      expect(wrapper).not.toRender(FeatureComponent);
-    });
-  });
-});
-
-describe('when feature enabled', () => {
-  let wrapper;
-
-  describe('with `children`', () => {
-    describe('being a `node`', () => {
-      beforeEach(() => {
-        wrapper = shallow(
-          <FeatureToggled
-            isFeatureEnabled
-            untoggledComponent={UntoggledComponent}
-          >
-            <FeatureComponent />
-          </FeatureToggled>
-        );
-      });
-
-      it('should match snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should not render the `UntoggledComponent`', () => {
-        expect(wrapper).not.toRender(UntoggledComponent);
-      });
-
-      it('should render the `FeatureComponent`', () => {
-        expect(wrapper).toRender(FeatureComponent);
-      });
-    });
-
-    describe('being a `function`', () => {
-      let props;
-      beforeEach(() => {
-        props = {
-          isFeatureEnabled: true,
-          untoggledComponent: UntoggledComponent,
-          children: jest.fn(() => <div>FeatureComponent</div>),
-        };
-
-        wrapper = shallow(<FeatureToggled {...props} />);
-      });
-
-      it('should match snapshot', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-
-      it('should invoke `children`', () => {
-        expect(props.children).toHaveBeenCalled();
-      });
-
-      it('should invoke `children` with `isFeatureEnabled`', () => {
-        expect(props.children).toHaveBeenCalledWith({
-          isFeatureEnabled: props.isFeatureEnabled,
-        });
-      });
-    });
-  });
-
-  describe('with `toggledComponent`', () => {
-    beforeEach(() => {
-      wrapper = shallow(
-        <FeatureToggled
-          isFeatureEnabled
-          untoggledComponent={UntoggledComponent}
-          toggledComponent={FeatureComponent}
-        />
-      );
-    });
-
-    it('should match snapshot', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should not render the `UntoggledComponent`', () => {
-      expect(wrapper).not.toRender(UntoggledComponent);
-    });
-
-    it('should render the `FeatureComponent`', () => {
-      expect(wrapper).toRender(FeatureComponent);
-    });
-  });
-
-  describe('with `render`', () => {
-    let props;
-    beforeEach(() => {
-      props = {
-        isFeatureEnabled: true,
-        untoggledComponent: UntoggledComponent,
-        render: jest.fn(() => <div>FeatureComponent</div>),
-      };
-
-      wrapper = shallow(<FeatureToggled {...props} />);
-    });
-
-    it('should match snapshot', () => {
-      expect(wrapper).toMatchSnapshot();
-    });
-
-    it('should invoke `render`', () => {
-      expect(props.render).toHaveBeenCalled();
-    });
+  it('should warn about a deprecation', () => {
+    expect(warning).toHaveBeenCalled();
   });
 });
