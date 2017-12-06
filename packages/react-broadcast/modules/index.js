d7ef52343218963a2f842655f7e1e47272e2b223
tdeekens <nerd@tdeekens.name>
2017-12-06 21:31:12 +0100

    fix: export of ToggleFeature

--- packages/react-broadcast/modules/index.js
+++ packages/react-broadcast/modules/index.js
@@ -1,5 +1,6 @@
 export {
   FeatureToggled,
+  ToggleFeature,
   injectFeatureToggle,
   injectFeatureToggles,
   branchOnFeatureToggle,
--- packages/react-redux/modules/components/index.js
+++ packages/react-redux/modules/components/index.js
@@ -1,4 +1,5 @@
 export { default as FeatureToggled } from './feature-toggled';
+export { default as ToggleFeature } from './toggle-feature';
 export { default as injectFeatureToggle } from './inject-feature-toggle';
 export { default as injectFeatureToggles } from './inject-feature-toggles';
 export { default as branchOnFeatureToggle } from './branch-on-feature-toggle';
