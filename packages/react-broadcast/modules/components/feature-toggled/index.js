7451f4e139de5cec6933539f2f1e28c6a3f0993e
tdeekens <nerd@tdeekens.name>
2017-12-06 20:02:12 +0100

    fix: deprecation of FeatureToggled moving it into packages

--- packages/react-broadcast/modules/components/feature-toggled/index.js
+++ packages/react-broadcast/modules/components/feature-toggled/index.js
@@ -1,1 +1,15 @@
-export default from '../toggle-feature';
+import React from 'react';
+import PropTypes from 'prop-types';
+import warning from 'warning';
+import ToggleFeature from '../toggle-feature';
+
+export default class FeatureToggled extends React.PureComponent {
+  render() {
+    warning(
+      false,
+      '`<FeatureToggled />` has been deprecated, please us `<ToggleFeature />`'
+    );
+
+    return React.createElement(ToggleFeature, this.props);
+  }
+}
