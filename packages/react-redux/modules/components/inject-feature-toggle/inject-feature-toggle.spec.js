f461cd43561fa2f8652d1ae867faf0a807569023
tdeekens <nerd@tdeekens.name>
2017-12-10 19:26:27 +0100

    test(react-redux): add integration spec for injectFeatureToggle

---
+++ packages/react-redux/modules/components/inject-feature-toggle/inject-feature-toggle.spec.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { STATE_SLICE } from '../../store';
+import injectFeatureToggle from './inject-feature-toggle';
+import Configure from '../configure';
+import memoryAdapter from '@flopflip/memory-adapter';
+
+const FeatureComponent = () => <div />;
+FeatureComponent.displayName = 'FeatureComponent';
+
+const createTestProps = custom => ({
+  adapterArgs: {},
+
+  ...custom,
+});
+const createMockStore = configureStore();
+
+describe('without `propKey`', () => {
+  const InjectedComponent = injectFeatureToggle('flag1')(FeatureComponent);
+  let props;
+  let wrapper;
+
+  let Container;
+  describe('when feature is disabled', () => {
+    beforeEach(() => {
+      const store = createMockStore({
+        [STATE_SLICE]: { flags: { flag1: false } },
+      });
+      props = createTestProps();
+      Container = () => <InjectedComponent />;
+      wrapper = mount(
+        <Provider store={store}>
+          <Configure {...props} adapter={memoryAdapter}>
+            <Container />
+          </Configure>
+        </Provider>
+      );
+    });
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should have feature disabling prop', () => {
+      expect(wrapper.find(FeatureComponent)).toHaveProp(
+        'isFeatureEnabled',
+        false
+      );
+    });
+
+    describe('when enabling feature', () => {
+      beforeEach(() => {
+        const store = createMockStore({
+          [STATE_SLICE]: { flags: { flag1: true } },
+        });
+        props = createTestProps();
+        Container = () => <InjectedComponent />;
+        wrapper = mount(
+          <Provider store={store}>
+            <Configure {...props} adapter={memoryAdapter}>
+              <Container />
+            </Configure>
+          </Provider>
+        );
+      });
+
+      it('should match snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+
+      it('should have feature enabling prop', () => {
+        expect(wrapper.find(FeatureComponent)).toHaveProp(
+          'isFeatureEnabled',
+          true
+        );
+      });
+    });
+  });
+});
