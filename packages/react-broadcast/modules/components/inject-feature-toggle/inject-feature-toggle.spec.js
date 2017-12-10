000fe4a5de2a664829c9368c617d8f50ac73f7c5
tdeekens <nerd@tdeekens.name>
2017-12-10 16:32:34 +0100

    test(react-broadcast): add integration spec for injectFeatureToggle

---
+++ packages/react-broadcast/modules/components/inject-feature-toggle/inject-feature-toggle.spec.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import injectFeatureToggle from './inject-feature-toggle';
+import Configure from '../configure';
+import memoryAdapter, { updateFlags } from '@flopflip/memory-adapter';
+import { FLAGS_CHANNEL } from '../../constants';
+
+const FeatureComponent = () => <div />;
+FeatureComponent.displayName = 'FeatureComponent';
+
+const createTestProps = custom => ({
+  adapterArgs: {},
+
+  ...custom,
+});
+
+describe('without `propKey`', () => {
+  const InjectedComponent = injectFeatureToggle('flag1')(FeatureComponent);
+  let props;
+  let wrapper;
+
+  let Container;
+  describe('when feature is disabled', () => {
+    beforeEach(() => {
+      props = createTestProps();
+      Container = () => <InjectedComponent />;
+      wrapper = mount(
+        <Configure {...props} adapter={memoryAdapter}>
+          <Container />
+        </Configure>
+      );
+    });
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should have feature disabling prop', () => {
+      expect(wrapper.find(FeatureComponent)).toHaveProp(
+        'isFeatureEnabled',
+        false
+      );
+    });
+
+    describe('when enabling feature', () => {
+      beforeEach(() => {
+        updateFlags({ flag1: true });
+        wrapper.update();
+      });
+
+      it('should match snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+
+      it('should have feature enabling prop', () => {
+        expect(wrapper.find(FeatureComponent)).toHaveProp(
+          'isFeatureEnabled',
+          true
+        );
+      });
+    });
+  });
+});
+
+describe('with `propKey`', () => {
+  const InjectedComponent = injectFeatureToggle('flag1', 'fooBar')(
+    FeatureComponent
+  );
+  let props;
+  let wrapper;
+
+  let Container;
+  beforeEach(() => {
+    props = createTestProps();
+    Container = () => <InjectedComponent />;
+    wrapper = mount(
+      <Configure {...props} adapter={memoryAdapter}>
+        <Container />
+      </Configure>
+    );
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have feature disabling `propKey`', () => {
+    expect(wrapper.find(FeatureComponent)).toHaveProp('fooBar', false);
+  });
+});
