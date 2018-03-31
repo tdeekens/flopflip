0514f45cb89b69b0bc115ae9b05dbadd61fb927e
tdeekens <nerd@tdeekens.name>
2018-03-31 19:20:12 +0200

    feat(react-broadcast): export reconfigure-adapter from react

--- packages/react-broadcast/modules/components/index.js
+++ packages/react-broadcast/modules/components/index.js
@@ -5,3 +5,4 @@ export { default as injectFeatureToggles } from './inject-feature-toggles';
 export { default as injectFeatureToggle } from './inject-feature-toggle';
 export { default as branchOnFeatureToggle } from './branch-on-feature-toggle';
 export { default as ConfigureFlopFlip } from './configure';
+export { default as ReconfigureFlopFlip } from './reconfigure';
