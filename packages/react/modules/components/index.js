b4747f50fa76bf9d3a5ce449b56c1237e5ef236d
tdeekens <nerd@tdeekens.name>
2018-03-31 19:19:36 +0200

    feat(react): export reconfigure-adapter through top-level module

--- packages/react/modules/components/index.js
+++ packages/react/modules/components/index.js
@@ -4,3 +4,4 @@ export { default as injectFeatureToggle } from './inject-feature-toggle';
 export { default as injectFeatureToggles } from './inject-feature-toggles';
 export { default as branchOnFeatureToggle } from './branch-on-feature-toggle';
 export { default as ConfigureAdapter } from './configure-adapter';
+export { default as ReconfigureAdapter } from './reconfigure-adapter';
