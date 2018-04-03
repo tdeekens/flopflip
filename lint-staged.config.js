aa6b3f058de00f1a8c532f84e72ddda478c78631
suǝʞǝǝpʇ <nerd@tdeekens.name>
2018-04-03 21:36:58 +0200

    chore(lint-staged): all flow check

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   'demo/**/*.js': ['npm run fix:eslint', 'npm run format:js', 'git add'],
   'packages/**/*.js': ['npm run fix:eslint', 'npm run format:js', 'git add'],
+  'packages/**/*.js': ['flow focus-check'],
   'packages/**/*.ts': ['npm run format:ts', 'git add'],
   '*.md': ['npm run format:md', 'git add'],
 };
