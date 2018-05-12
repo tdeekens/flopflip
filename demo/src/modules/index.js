f67500586929ea69bd1bb74a8674bc5d9dc70ae4
tdeekens <nerd@tdeekens.name>
2018-05-12 15:11:01 +0200

    chore(demo): use new createFlopflipReducer

--- demo/src/modules/index.js
+++ demo/src/modules/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
-import { flopflipReducer, FLOPFLIP_STATE_SLICE } from '@flopflip/react-redux';
+import {
+  createFlopflipReducer,
+  FLOPFLIP_STATE_SLICE,
+} from '@flopflip/react-redux';
 import counter from './counter';
 
 export default combineReducers({
-  [FLOPFLIP_STATE_SLICE]: flopflipReducer,
+  [FLOPFLIP_STATE_SLICE]: createFlopflipReducer(),
   counter,
 });
