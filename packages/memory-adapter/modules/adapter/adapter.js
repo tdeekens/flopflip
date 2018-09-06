7c90c2a5e01b2c926f71f9e207f7f5a777cf5a2e
tdeekens <tobias.deekens@commercetools.de>
2018-09-06 19:06:19 +0200

    refactor(memory-adapter): to reset and have better structured tests

--- packages/memory-adapter/modules/adapter/adapter.js
+++ packages/memory-adapter/modules/adapter/adapter.js
@@ -1,18 +1,25 @@
 // @flow
+import invariant from 'invariant';
 
 import type {
   User,
   AdapterState,
   AdapterArgs,
+  FlagName,
+  Flag,
   Flags,
   OnStatusStateChangeCallback,
   OnFlagsStateChangeCallback,
 } from '@flopflip/types';
 
-const adapterState: AdapterState = {
+const intialAdapterState: AdapterState = {
   isReady: false,
   flags: {},
   user: {},
+};
+
+let adapterState: AdapterState = {
+  ...intialAdapterState,
 };
 
 const configure = ({
@@ -27,6 +34,7 @@ const configure = ({
     adapterState.isConfigured = true;
     adapterState.isReady = true;
     adapterState.flags = {};
+
     updateUser(user);
 
     adapterState.onFlagsStateChange = onFlagsStateChange;
@@ -49,8 +57,9 @@ const reconfigure = ({ user }: { user: User }): Promise<any> => {
 const getIsReady = (): boolean => adapterState.isReady;
 
 const reset = (): void => {
-  adapterState.flags = adapterState.defaultFlags;
-  adapterState.onFlagsStateChange(adapterState.defaultFlags);
+  adapterState = {
+    ...intialAdapterState,
+  };
 };
 
 const updateUser = (user: User): User => {
@@ -58,6 +67,17 @@ const updateUser = (user: User): User => {
 };
 
 export const updateFlags = (flags: Flags): void => {
+  const isAdapterReady = Boolean(
+    adapterState.isConfigured && adapterState.isReady
+  );
+
+  invariant(
+    isAdapterReady,
+    '@flopflip/launchdarkly-memory: adapter not ready and configured. Flags can not be updated before.'
+  );
+
+  if (!isAdapterReady) return;
+
   adapterState.flags = {
     ...adapterState.flags,
     ...flags,
@@ -68,8 +88,12 @@ export const updateFlags = (flags: Flags): void => {
 
 export const getUser = (): User => adapterState.user;
 
+const getFlag = (flagName: FlagName): ?Flag =>
+  adapterState.flags && adapterState.flags[flagName];
+
 export default {
   getIsReady,
+  getFlag,
   reset,
   configure,
   reconfigure,
--- packages/memory-adapter/modules/adapter/adapter.spec.js
+++ packages/memory-adapter/modules/adapter/adapter.spec.js
@@ -1,101 +1,118 @@
 import ldClient from 'ldclient-js';
+import invariant from 'invariant';
 import adapter, { getUser, updateFlags } from './adapter';
+
+jest.mock('invariant');
 
 const createAdapterArgs = (customArgs = {}) => ({
   user: { id: 'foo' },
-  defaultFlags: { defaultFlag: true },
   onFlagsStateChange: jest.fn(),
   onStatusStateChange: jest.fn(),
 
   ...customArgs,
 });
 
-describe('when not configured', () => {
+describe('when configuring', () => {
+  const updatedFlags = { fooFlag: true, barFlag: false };
+  let adapterArgs;
+
+  beforeEach(() => {
+    invariant.mockClear();
+    adapterArgs = createAdapterArgs();
+  });
+
   it('should indicate that the adapter is not ready', () => {
     expect(adapter.getIsReady()).toBe(false);
   });
-});
 
-describe('when configuring', () => {
-  let adapterArgs = createAdapterArgs();
-
-  beforeEach(() => adapter.configure(adapterArgs));
-
-  it('should indicate that the adapter is ready', () => {
-    expect(adapter.getIsReady()).toBe(true);
-  });
-
-  it('should invoke `onStatusStateChange`', () => {
-    expect(adapterArgs.onStatusStateChange).toHaveBeenCalled();
-  });
-
-  it('should invoke `onStatusStateChange` with `isReady`', () => {
-    expect(adapterArgs.onStatusStateChange).toHaveBeenCalledWith({
-      isReady: true,
-    });
-  });
-
-  it('should invoke `onFlagsStateChange`', () => {
-    expect(adapterArgs.onFlagsStateChange).toHaveBeenCalled();
-  });
-
-  describe('when reconfiguring', () => {
-    const user = { id: 'bar' };
-
-    beforeEach(() => adapter.reconfigure({ user }));
-
-    it('should update the user', () => {
-      expect(getUser()).toEqual(user);
-    });
-
-    it('should invoke `onFlagsStateChange`', () => {
-      expect(adapterArgs.onFlagsStateChange).toHaveBeenCalled();
-    });
-
-    it('should invoke `onFlagsStateChange` with empty flags', () => {
-      expect(adapterArgs.onFlagsStateChange).toHaveBeenCalledWith({});
-    });
-  });
-
-  describe('when updating flags', () => {
-    const updatedFlags = { fooFlag: true, barFlag: false };
-
+  describe('updating flags', () => {
     beforeEach(() => {
-      // From `configure`
-      adapterArgs.onFlagsStateChange.mockClear();
+      updateFlags({ attempted: 'flagUpdate' });
+    });
 
-      updateFlags(updatedFlags);
+    it('should invoke and trigger `invariant`', () => {
+      expect(invariant).toHaveBeenCalledWith(false, expect.any(String));
+    });
+  });
+
+  describe('when configured', () => {
+    beforeEach(() => adapter.configure(adapterArgs));
+
+    it('should indicate that the adapter is ready', () => {
+      expect(adapter.getIsReady()).toBe(true);
+    });
+
+    it('should invoke `onStatusStateChange`', () => {
+      expect(adapterArgs.onStatusStateChange).toHaveBeenCalled();
+    });
+
+    it('should invoke `onStatusStateChange` with `isReady`', () => {
+      expect(adapterArgs.onStatusStateChange).toHaveBeenCalledWith({
+        isReady: true,
+      });
     });
 
     it('should invoke `onFlagsStateChange`', () => {
       expect(adapterArgs.onFlagsStateChange).toHaveBeenCalled();
     });
 
-    it('should invoke `onFlagsStateChange` with `updatedFlags`', () => {
-      expect(adapterArgs.onFlagsStateChange).toHaveBeenCalledWith(
-        expect.objectContaining(updatedFlags)
-      );
-    });
-
-    it('should invoke `onFlagsStateChange` with `defaultFlags`', () => {
-      expect(adapterArgs.onFlagsStateChange).toHaveBeenCalledWith(
-        expect.objectContaining(adapterArgs.defaultFlags)
-      );
-    });
-
-    describe('when resetting', () => {
+    describe('when updating flags', () => {
       beforeEach(() => {
+        // From `configure`
         adapterArgs.onFlagsStateChange.mockClear();
-
-        adapter.reset();
 
         updateFlags(updatedFlags);
       });
 
-      it('should invoke `onFlagsStateChange` with `defaultFlags`', () => {
+      it('should invoke but not trigger `invariant`', () => {
+        expect(invariant).toHaveBeenCalledWith(true, expect.any(String));
+      });
+
+      it('should invoke `onFlagsStateChange`', () => {
+        expect(adapterArgs.onFlagsStateChange).toHaveBeenCalled();
+      });
+
+      it('should invoke `onFlagsStateChange` with `updatedFlags`', () => {
         expect(adapterArgs.onFlagsStateChange).toHaveBeenCalledWith(
-          adapterArgs.defaultFlags
+          expect.objectContaining(updatedFlags)
         );
+      });
+    });
+
+    describe('when reconfiguring', () => {
+      const user = { id: 'bar' };
+
+      beforeEach(() => adapter.reconfigure({ user }));
+
+      it('should update the user', () => {
+        expect(getUser()).toEqual(user);
+      });
+
+      it('should invoke `onFlagsStateChange`', () => {
+        expect(adapterArgs.onFlagsStateChange).toHaveBeenCalled();
+      });
+
+      it('should invoke `onFlagsStateChange` with empty flags', () => {
+        expect(adapterArgs.onFlagsStateChange).toHaveBeenCalledWith({});
+      });
+    });
+
+    describe('when resetting', () => {
+      beforeEach(() => {
+        updateFlags(updatedFlags);
+
+        adapterArgs.onFlagsStateChange.mockClear();
+
+        adapter.reset();
+      });
+
+      it('should invoke not `onFlagsStateChange`', () => {
+        expect(adapterArgs.onFlagsStateChange).not.toHaveBeenCalled();
+      });
+
+      it('should reset the flags', () => {
+        expect(adapter.getFlag(updatedFlags.fooFlag)).not.toBeDefined();
+        expect(adapter.getFlag(updatedFlags.barFlag)).not.toBeDefined();
       });
     });
   });
