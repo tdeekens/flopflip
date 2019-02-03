1806c01e244d245888dc04ac118d61fa5c1e2144
tdeekens <nerd@tdeekens.name>
2019-02-03 17:04:19 +0100

    fix: migrate all index.js files to ts

---
+++ packages/react-broadcast/modules/index.ts
@@ -0,0 +1,11 @@
+export {
+ToggleFeature,
+injectFeatureToggle,
+injectFeatureToggles,
+branchOnFeatureToggle,
+ConfigureFlopFlip,
+ReconfigureFlopFlip,
+} from './components';
+export { useFeatureToggle } from './hooks';
+
+export { version } from '../package.json';
