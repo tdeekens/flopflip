53ff7587c8cd1849d1f64582a7e81aa7e6f47e13
tdeekens <nerd@tdeekens.name>
2019-02-03 16:55:55 +0100

    chore: add pretty-quick

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   'demo/**/*.js': ['npm run fix:eslint', 'npm run format', 'git add'],
-  'packages/**/*.js': ['npm run fix:eslint', 'npm run format', 'git add'],
-  'packages/**/*.ts': ['npm run format', 'git add'],
+  'packages/**/*.js': [
+    'npm run fix:eslint',
+    'pretty-quick --staged',
+    'npm run format',
+    'git add',
+  ],
+  'packages/**/*.ts': ['pretty-quick --staged', 'npm run format', 'git add'],
   '*.md': ['npm run format:md', 'git add'],
   '*.yaml': ['npm run format:yaml', 'git add'],
 };
