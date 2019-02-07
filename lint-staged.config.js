3e084bd65dd9f48398d0eb6dd9de020366bdfe12
tdeekens <nerd@tdeekens.name>
2019-02-07 20:41:41 +0100

    chore: remove pretty-quick again

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  'demo/**/*.js': ['npm run fix:eslint', 'npm run format', 'git add -u'],
-  'packages/**/*.js': [
+  'demo/**/*.js': ['npm run fix:eslint', 'npm run format:ts', 'git add -u'],
+  'packages/**/*.{js,ts}': [
     'npm run fix:eslint',
-    'pretty-quick --staged',
-    'npm run format',
+    'npm run format:ts',
     'git add -u',
   ],
-  'packages/**/*.ts': ['pretty-quick --staged', 'npm run format', 'git add -u'],
   '*.md': ['npm run format:md', 'git add -u'],
   '*.yaml': ['npm run format:yaml', 'git add -u'],
 };
