ad1c276f20d473fd95288c5bec79951c5976aad4
suǝʞǝǝpʇ <nerd@tdeekens.name>
2019-02-23 17:12:52 +0100

    fix: improve typings (#602)

--- packages/react/modules/hocs/wrap-display-name/wrap-display-name.ts
+++ packages/react/modules/hocs/wrap-display-name/wrap-display-name.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default <Props extends object>(
-  BaseComponent: React.ComponentType<Props>,
+export default (
+  BaseComponent: React.ComponentType<any>,
   hocName: string
 ): string => {
   const previousDisplayName = BaseComponent.displayName || BaseComponent.name;
