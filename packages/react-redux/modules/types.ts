2f36ad2ab79e25857cca6fc3e2ed8a8899765f7a
Jan Prieser <jaypea@users.noreply.github.com>
2019-05-23 11:56:06 +0200

    fix(react-redux): state types in react-redux (#691)

--- packages/react-redux/modules/types.ts
+++ packages/react-redux/modules/types.ts
@@ -1,10 +1,13 @@
 import { Flags, AdapterStatus } from '@flopflip/types';
 import { UpdateStatusAction } from './ducks/status/types';
 import { UpdateFlagsAction } from './ducks/flags/types';
+import { STATE_SLICE } from './store';
 
 export type State = {
-  flags?: Flags;
-  status?: AdapterStatus;
+  [STATE_SLICE]: {
+    flags?: Flags;
+    status?: AdapterStatus;
+  };
 };
 
 export type UpdateFlagsAction = UpdateFlagsAction;
