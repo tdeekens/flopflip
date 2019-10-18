116c58b4e85659629dc075e254d57f2caf2e3b39
suǝʞǝǝpʇ <nerd@tdeekens.name>
2019-10-18 20:59:17 +0200

    chore: remove duplicate warning (#843)

--- packages/react-broadcast/modules/hooks/use-feature-toggle/use-feature-toggle.ts
+++ packages/react-broadcast/modules/hooks/use-feature-toggle/use-feature-toggle.ts
@@ -1,6 +1,4 @@
 import React from 'react';
-import camelCase from 'lodash/camelCase';
-import warning from 'tiny-warning';
 import { getIsFeatureEnabled } from '@flopflip/react';
 import { FlagName, Flags, FlagVariation } from '@flopflip/types';
 import { FlagsContext } from '../../components/flags-context';
@@ -9,11 +7,6 @@ export default function useFeatureToggle(
   flagName: FlagName,
   flagVariation: FlagVariation = true
 ): Error | boolean {
-  warning(
-    flagName === camelCase(flagName),
-    '@flopflip/react-broadcast: passed flag name does not seem to be normalized which may result in unexpected toggling. Please refer to our readme for more information: https://github.com/tdeekens/flopflip#flag-normalization'
-  );
-
   if (typeof React.useContext === 'function') {
     const flags: Flags = React.useContext(FlagsContext);
     const isFeatureEnabled: boolean = getIsFeatureEnabled(
