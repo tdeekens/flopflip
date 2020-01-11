6dc597dce3f2810b41572f6c74775afd8dbdcbcf
tdeekens <tobias.deekens@commercetools.de>
2020-01-11 16:22:38 +0100

    refactor(react): to remove unused code

--- packages/react/modules/components/index.ts
+++ packages/react/modules/components/index.ts
@@ -3,8 +3,6 @@ export {
   createAdapterContext,
 } from './adapter-context';
 export { default as ToggleFeature } from './toggle-feature';
-export { default as injectFeatureToggle } from './inject-feature-toggle';
-export { default as injectFeatureToggles } from './inject-feature-toggles';
 export { default as branchOnFeatureToggle } from './branch-on-feature-toggle';
 export { default as ConfigureAdapter } from './configure-adapter';
 export { default as ReconfigureAdapter } from './reconfigure-adapter';
