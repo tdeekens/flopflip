8a8da49ea6a807c8dacfaa05a870c67d9285fde4
tdeekens <tobias.deekens@commercetools.de>
2020-02-09 14:21:01 +0100

    refactor: remove Types postfix

--- packages/react-redux/modules/hooks/use-adapter-status/use-adapter-status.ts
+++ packages/react-redux/modules/hooks/use-adapter-status/use-adapter-status.ts
@@ -1,10 +1,10 @@
-import { AdapterStatus as AdapterStatusType } from '@flopflip/types';
+import { TAdapterStatus } from '@flopflip/types';
 
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectStatus } from '../../ducks/status';
 
-export default function useAdapterStatus(): AdapterStatusType {
+export default function useAdapterStatus(): TAdapterStatus {
   const status = useSelector(selectStatus);
 
   React.useDebugValue(status);
