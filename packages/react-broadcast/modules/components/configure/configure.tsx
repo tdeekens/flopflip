404a46d0adacd83e453487fc621dd8a436f59a16
tdeekens <tobias.deekens@commercetools.de>
2020-02-12 01:43:58 +0100

    fix: revert to class component

--- packages/react-broadcast/modules/components/configure/configure.tsx
+++ packages/react-broadcast/modules/components/configure/configure.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ConfigureAdapter, useIsMounted } from '@flopflip/react';
+import { ConfigureAdapter } from '@flopflip/react';
 import {
   TFlags,
   TAdapter,
@@ -22,58 +22,73 @@ type State = {
   configurationId?: string;
 };
 
-const Configure = <AdapterInstance extends TAdapter>(
-  props: Props<AdapterInstance>
-) => {
-  const isMounted = useIsMounted();
-  const [flags, setFlags] = React.useState<State['flags']>({});
-  const [status, setStatus] = React.useState<State['status']>({});
+export default class Configure<
+  AdapterInstance extends TAdapter
+> extends React.PureComponent<Props<AdapterInstance>, State> {
+  static displayName = 'ConfigureFlopflip';
 
-  const handleUpdateFlags = React.useCallback(
-    (flags: TFlags) => {
-      if (isMounted.current) {
-        setFlags(prevFlags => ({
-          ...prevFlags,
-          ...flags,
-        }));
-      }
+  static defaultProps = {
+    defaultFlags: {},
+    shouldDeferAdapterConfiguration: false,
+  };
+
+  state: State = {
+    flags: {},
+    status: {
+      isReady: false,
+      isConfigured: false,
     },
-    [isMounted, setFlags]
-  );
+  };
 
-  const handleUpdateStatus = React.useCallback(
-    (status: TAdapterStatus) => {
-      if (isMounted.current) {
-        setStatus(prevStatus => ({
-          ...prevStatus,
+  isUnmounted = false;
+
+  componentDidMount(): void {
+    this.isUnmounted = false;
+  }
+
+  componentWillUnmount(): void {
+    this.isUnmounted = true;
+  }
+
+  handleUpdateFlags = (nextFlags: TFlags) => {
+    if (!this.isUnmounted) {
+      this.setState(prevState => ({
+        flags: {
+          ...prevState.flags,
+          ...nextFlags,
+        },
+      }));
+    }
+  };
+
+  handleUpdateStatus = (status: TAdapterStatus) => {
+    if (!this.isUnmounted) {
+      this.setState(prevState => ({
+        status: {
+          ...prevState.status,
           ...status,
-        }));
-      }
-    },
-    [isMounted, setStatus]
-  );
+        },
+      }));
+    }
+  };
 
-  return (
-    <FlagsContext.Provider value={flags}>
-      <ConfigureAdapter
-        adapter={props.adapter}
-        adapterArgs={props.adapterArgs}
-        adapterStatus={status}
-        defaultFlags={props.defaultFlags}
-        shouldDeferAdapterConfiguration={props.shouldDeferAdapterConfiguration}
-        onFlagsStateChange={handleUpdateFlags}
-        onStatusStateChange={handleUpdateStatus}
-      >
-        {props.children}
-      </ConfigureAdapter>
-    </FlagsContext.Provider>
-  );
-};
-
-Configure.displayName = 'ConfigureFlopflip';
-Configure.defaultProps = {
-  defaultFlags: {},
-  shouldDeferAdapterConfiguration: false,
-};
-
-export default Configure;
+  render(): React.ReactNode {
+    return (
+      <FlagsContext.Provider value={this.state.flags}>
+        <ConfigureAdapter
+          adapter={this.props.adapter}
+          adapterArgs={this.props.adapterArgs}
+          adapterStatus={this.state.status}
+          defaultFlags={this.props.defaultFlags}
+          shouldDeferAdapterConfiguration={
+            this.props.shouldDeferAdapterConfiguration
+          }
+          onFlagsStateChange={this.handleUpdateFlags}
+          onStatusStateChange={this.handleUpdateStatus}
+        >
+          {this.props.children}
+        </ConfigureAdapter>
+      </FlagsContext.Provider>
+    );
+  }
+}
