4a6ab9902f420062cea57e3a6cf700245fcdfab2
tdeekens <tobias.deekens@commercetools.de>
2020-02-13 10:00:40 +0100

    fix: add comment

--- packages/react/modules/hooks/use-adapter-subscription/use-adapter-subscription.ts
+++ packages/react/modules/hooks/use-adapter-subscription/use-adapter-subscription.ts
@@ -2,6 +2,15 @@ import { TAdapter, TAdapterSubscriptionStatus } from '@flopflip/types';
 import React from 'react';
 
 function useAdapterSubscription(adapter: TAdapter) {
+  /**
+   * NOTE:
+   *    This state needs to be duplicated in a React.ref
+   *    as under test multiple instances of flopflip might
+   *    be rendered. This yields in them competing in adapter
+   *    subscription state (e.g. A unsubscribing and B subscribing
+   *    which yields A and B being subscribed as the adapter
+   *    is a singleton).
+   */
   const useAdapterSubscriptionStatusRef = React.useRef(
     TAdapterSubscriptionStatus.Subscribed
   );
