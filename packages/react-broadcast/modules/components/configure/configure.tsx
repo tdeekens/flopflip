c0c3ebcf902c3a95ba8059c6c625a9d1201c0748
Tobias Deekens <nerd@tdeekens.name>
2020-02-26 15:23:10 +0100

    refactor: to use debug value more (#1000)
    * refactor: to use debug value more
    
    * fix: typescript types

--- packages/react-broadcast/modules/components/configure/configure.tsx
+++ packages/react-broadcast/modules/components/configure/configure.tsx
@@ -32,13 +32,41 @@ const initialAdapterStatus: State['status'] = {
 };
 const initialFlags: State['flags'] = {};
 
-const Configure = <AdapterInstance extends TAdapter>(
-  props: Props<AdapterInstance>
-) => {
+const useFlagsState = ({
+  initialFlags,
+}: {
+  initialFlags: State['flags'];
+}): [TFlags, React.Dispatch<React.SetStateAction<TFlags>>] => {
   const [flags, setFlags] = React.useState<State['flags']>(initialFlags);
+
+  React.useDebugValue({
+    flags,
+  });
+
+  return [flags, setFlags];
+};
+
+const useStatusState = ({
+  initialAdapterStatus,
+}: {
+  initialAdapterStatus: State['status'];
+}): [TAdapterStatus, React.Dispatch<React.SetStateAction<TAdapterStatus>>] => {
   const [status, setStatus] = React.useState<State['status']>(
     initialAdapterStatus
   );
+
+  React.useDebugValue({
+    status,
+  });
+
+  return [status, setStatus];
+};
+
+const Configure = <AdapterInstance extends TAdapter>(
+  props: Props<AdapterInstance>
+) => {
+  const [flags, setFlags] = useFlagsState({ initialFlags });
+  const [status, setStatus] = useStatusState({ initialAdapterStatus });
 
   // NOTE:
   //   Using this prevents the callbacks being invoked
