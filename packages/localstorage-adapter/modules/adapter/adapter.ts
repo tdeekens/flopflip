9b26680bf795219f6493cd74875b17e28d750816
tdeekens <tobias.deekens@commercetools.com>
2020-05-21 13:13:14 +0200

    fix: remove ts-ignore

--- packages/localstorage-adapter/modules/adapter/adapter.ts
+++ packages/localstorage-adapter/modules/adapter/adapter.ts
@@ -43,7 +43,6 @@ const intialAdapterState: TAdapterStatus & LocalStorageAdapterState = {
   user: {},
   // Typings are incorrect and state that mitt is not callable.
   // Value of type 'MittStatic' is not callable. Did you mean to include 'new'
-  // @ts-ignore
   emitter: mitt(),
 };
 
--- packages/memory-adapter/modules/adapter/adapter.ts
+++ packages/memory-adapter/modules/adapter/adapter.ts
@@ -35,7 +35,6 @@ const intialAdapterState: TAdapterStatus & MemoryAdapterState = {
   user: {},
   // Typings are incorrect and state that mitt is not callable.
   // Value of type 'MittStatic' is not callable. Did you mean to include 'new'
-  // @ts-ignore
   emitter: mitt(),
 };
 
