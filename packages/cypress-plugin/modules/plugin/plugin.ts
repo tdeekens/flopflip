cbbba1e9478b50a0f0bc8f88266e065baaf907e5
tdeekens <tobias.deekens@commercetools.com>
2020-08-19 15:35:29 +0200

    fix(cypress-plugin): types

--- packages/cypress-plugin/modules/plugin/plugin.ts
+++ packages/cypress-plugin/modules/plugin/plugin.ts
@@ -1,14 +1,49 @@
-import type { TFlags } from '@flopflip/types';
+/// <reference types="cypress" />
 
-const state = {};
+import type {
+  TFlags,
+  TLaunchDarklyAdapterInterface,
+  TLocalStorageAdapterInterface,
+  TMemoryAdapterInterface,
+  TSplitioAdapterInterface,
+} from '@flopflip/types';
+
+type TCypressPluginState = {
+  adapter?:
+    | TLaunchDarklyAdapterInterface
+    | TLocalStorageAdapterInterface
+    | TMemoryAdapterInterface
+    | TSplitioAdapterInterface;
+  updateFlags?: (flags: TFlags) => void;
+};
+declare namespace Cypress {
+  interface Chainable<Subject> {
+    updateFeatureFlags: (flags: TFlags) => Chainable<Subject>;
+  }
+}
+
+const state: TCypressPluginState = {
+  adapter: undefined,
+  updateFlags: undefined,
+};
 
 const addCommands = () => {
   Cypress.Commands.add('updateFeatureFlags', (flags: TFlags) => {
+    if (!state.updateFlags) {
+      throw new Error(
+        '@flopflip/cypress: `updateFlags` is not defined. Pass it when installing the plugin.'
+      );
+    }
+
     state.updateFlags(flags);
   });
 };
 
-const install = (on, adapter, updateFlags) => {
+const install = (
+  _on,
+  adapter: TCypressPluginState['adapter'],
+  updateFlags: TCypressPluginState['updateFlags']
+) => {
   state.adapter = adapter;
   state.updateFlags = updateFlags;
 };
