a5e6c128f01af09ff14dc295e7ea1a05474a6be5
Tobias Deekens <nerd@tdeekens.name>
2020-08-23 18:51:15 +0200

    fix(cypress-plugin): to wait for global (#1216)

---
+++ .changeset/giant-paws-serve.md
@@ -0,0 +1,5 @@
+---
+"@flopflip/cypress-plugin": patch
+---
+
+fix(cypress-plugin): to wait for global
--- packages/cypress-plugin/modules/plugin/plugin.ts
+++ packages/cypress-plugin/modules/plugin/plugin.ts
@@ -12,32 +12,35 @@ declare namespace Cypress {
   }
 }
 
+const FLOPFLIP_GLOBAL = '__flopflip__';
+
 const addCommands = (options: TCypressPluginAddCommandOptions) => {
   Cypress.Commands.add('updateFeatureFlags', (flags: TFlags) => {
-    cy.window().then((win) => {
-      const flopFlipGlobal = win.__flopflip__;
-      const flopflipAdapterGlobal = flopFlipGlobal[options.adapterId];
+    cy.window()
+      .its(FLOPFLIP_GLOBAL)
+      .then((flopFlipGlobal) => {
+        const flopflipAdapterGlobal = flopFlipGlobal[options.adapterId];
 
-      if (!flopflipAdapterGlobal) {
-        throw new Error(
-          '@flopflip/cypress: namespace or adapter of the passed id does not exist. Make sure you use one and the specified adapter.'
-        );
-      }
+        if (!flopflipAdapterGlobal) {
+          throw new Error(
+            '@flopflip/cypress: namespace or adapter of the passed id does not exist. Make sure you use one and the specified adapter.'
+          );
+        }
 
-      Cypress.log({
-        name: 'updateFeatureFlags',
-        message: 'Updating @flopflip feature flags.',
-        consoleProps: () => {
-          return {
-            flags,
-          };
-        },
+        Cypress.log({
+          name: 'updateFeatureFlags',
+          message: 'Updating @flopflip feature flags.',
+          consoleProps: () => {
+            return {
+              flags,
+            };
+          },
+        });
+
+        flopflipAdapterGlobal?.updateFlags(flags, {
+          unsubscribeFlags: true,
+        });
       });
-
-      flopflipAdapterGlobal?.updateFlags(flags, {
-        unsubscribeFlags: true,
-      });
-    });
   });
 };
 
