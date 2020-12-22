33b3216f227969f8a5ce0670b9590e5e06243fea
Tobias Deekens <nerd@tdeekens.name>
2020-12-22 14:58:29 +0100

    feat: to have localstorage-cache package (#1348)

---
+++ packages/localstorage-cache/src/cache/cache.ts
@@ -0,0 +1,30 @@
+import { TCacheOptions, TCache } from '@flopflip/types';
+
+const createCache = (options: TCacheOptions) => {
+  const cache: TCache = {
+    get: (key) => {
+      const localStorageValue = localStorage.getItem(
+        `${options.prefix}__${key}`
+      );
+
+      return localStorageValue ? JSON.parse(localStorageValue) : null;
+    },
+    set: (key, value) => {
+      try {
+        localStorage.setItem(
+          `${options.prefix}__${key}`,
+          JSON.stringify(value)
+        );
+        return true;
+        // eslint-disable-next-line
+      } catch (_error) {
+        return false;
+      }
+    },
+    unset: (key) => localStorage.removeItem(`${options.prefix}__${key}`),
+  };
+
+  return cache;
+};
+
+export default createCache;
---
+++ packages/localstorage-cache/src/cache/index.ts
@@ -0,0 +1,4 @@
+const version = '__@FLOPFLIP/VERSION_OF_RELEASE__';
+
+export { version };
+export { default } from './cache';
