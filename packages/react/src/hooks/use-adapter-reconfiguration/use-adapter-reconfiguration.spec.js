d5f63758ada4eed23507361007b687ff6f1c674b
tdeekens <tobias.deekens@commercetools.com>
2020-12-24 16:42:38 +0100

    fix: test

--- packages/react/src/hooks/use-adapter-reconfiguration/use-adapter-reconfiguration.spec.js
+++ packages/react/src/hooks/use-adapter-reconfiguration/use-adapter-reconfiguration.spec.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import useAdapterReconfiguration from './use-adapter-reconfiguration';
 
 const reconfigure = jest.fn();
 
 it('should return a function', () => {
-  useContext = jest.fn(() => ({ reconfigure }));
+  React.useContext = jest.fn(() => ({ reconfigure }));
   expect(useAdapterReconfiguration()).toBe(reconfigure);
 });
