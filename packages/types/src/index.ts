53fda93cdc1271c39fb11cd136edb074f012e7d6
tdeekens <tobias.deekens@commercetools.com>
2021-01-14 15:14:01 +0100

    fix: return type of execute

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -80,7 +80,7 @@ export type TGraphQlAdapterArgs<
 export type THttpAdapterArgs<
   TAdditionalUserProperties = TDefaultAdditionalUserProperties
 > = TBaseAdapterArgs<TAdditionalUserProperties> & {
-  execute: <TParsedFlags = TFlags>() => Promise<TParsedFlags>;
+  execute: <TParsedFlags = any>() => TParsedFlags;
   pollingInteralMs?: number;
   cacheIdentifier?: TCacheIdentifiers;
 };
