296b1f0ba94f8e0096a54648c98a58d1de52a5fe
tdeekens <tobias.deekens@commercetools.com>
2021-12-09 12:28:11 +0100

    fix: to not call hook conditionally

--- packages/react-redux/src/hooks/use-flag-variation/use-flag-variation.ts
+++ packages/react-redux/src/hooks/use-flag-variation/use-flag-variation.ts
@@ -3,8 +3,6 @@ import { type TFlagName, type TFlagVariation } from '@flopflip/types';
 import useFlagVariations from '../use-flag-variations';
 
 export default function useFlagVariation(flagName?: TFlagName): TFlagVariation {
-  if (!flagName) return false;
-
   const [flagVariation] = useFlagVariations([flagName]);
 
   return flagVariation;
