dcd75d5f5d1f4e3f405e0b6086883e490fdef8ed
tdeekens <tobias.deekens@commercetools.com>
2021-12-09 12:22:27 +0100

    fix: to make flagName optional

--- packages/react-redux/src/hooks/use-flag-variation/use-flag-variation.ts
+++ packages/react-redux/src/hooks/use-flag-variation/use-flag-variation.ts
@@ -2,7 +2,9 @@ import { type TFlagName, type TFlagVariation } from '@flopflip/types';
 
 import useFlagVariations from '../use-flag-variations';
 
-export default function useFlagVariation(flagName: TFlagName): TFlagVariation {
+export default function useFlagVariation(flagName?: TFlagName): TFlagVariation {
+  if (!flagName) return false;
+
   const [flagVariation] = useFlagVariations([flagName]);
 
   return flagVariation;
