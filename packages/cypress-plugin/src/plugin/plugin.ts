461ecd87b734fc998ff3053cbd902ffe465a3d82
tdeekens <tobias.deekens@commercetools.com>
2021-12-27 15:12:20 +0100

    fix(cypress): plugin types

--- packages/cypress-plugin/src/plugin/plugin.ts
+++ packages/cypress-plugin/src/plugin/plugin.ts
@@ -9,39 +9,44 @@ type TCypressPluginAddCommandOptions = {
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
-  interface Chainable<Subject> {
-    updateFeatureFlags: (flags: TFlags) => Chainable<Subject>;
+  interface Chainable {
+    updateFeatureFlags: (flags: TFlags) => Chainable;
   }
 }
 
 const FLOPFLIP_GLOBAL = '__flopflip__';
 
 const addCommands = (options: TCypressPluginAddCommandOptions) => {
-  Cypress.Commands.add('updateFeatureFlags', (flags: TFlags) => {
-    cy.window()
-      .its(FLOPFLIP_GLOBAL)
-      .then((flopFlipGlobal) => {
-        const flopflipAdapterGlobal = flopFlipGlobal[options.adapterId];
+  Cypress.Commands.add(
+    // @ts-expect-error Cypress doesn't seem to allow a non any chainable
+    'updateFeatureFlags',
+    function flopflipCommand(flags: TFlags) {
+      return cy
+        .window()
+        .its(FLOPFLIP_GLOBAL)
+        .then((flopFlipGlobal) => {
+          const flopflipAdapterGlobal = flopFlipGlobal[options.adapterId];
 
-        if (!flopflipAdapterGlobal) {
-          throw new Error(
-            '@flopflip/cypress: namespace or adapter of the passed id does not exist. Make sure you use one and the specified adapter.'
-          );
-        }
+          if (!flopflipAdapterGlobal) {
+            throw new Error(
+              '@flopflip/cypress: namespace or adapter of the passed id does not exist. Make sure you use one and the specified adapter.'
+            );
+          }
 
-        Cypress.log({
-          name: 'updateFeatureFlags',
-          message: 'Updating @flopflip feature flags.',
-          consoleProps: () => ({
-            flags,
-          }),
+          Cypress.log({
+            name: 'updateFeatureFlags',
+            message: 'Updating @flopflip feature flags.',
+            consoleProps: () => ({
+              flags,
+            }),
+          });
+
+          flopflipAdapterGlobal?.updateFlags(flags, {
+            unsubscribeFlags: true,
+          });
         });
-
-        flopflipAdapterGlobal?.updateFlags(flags, {
-          unsubscribeFlags: true,
-        });
-      });
-  });
+    }
+  );
 };
 
 const install = (_on) => {
