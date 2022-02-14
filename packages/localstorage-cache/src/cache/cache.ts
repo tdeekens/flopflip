3cce98047f3a023a0ff1e67e83ddb85558794930
tdeekens <tobias.deekens@commercetools.com>
2022-02-14 20:58:05 +0100

    fix(lint): to match new rules

--- packages/localstorage-cache/src/cache/cache.ts
+++ packages/localstorage-cache/src/cache/cache.ts
@@ -2,14 +2,14 @@ import { TCache, TCacheOptions } from '@flopflip/types';
 
 const createCache = (options: TCacheOptions) => {
   const cache: TCache = {
-    get: (key) => {
+    get(key) {
       const localStorageValue = localStorage.getItem(
         [options.prefix, key].join('/')
       );
 
       return localStorageValue ? JSON.parse(localStorageValue) : null;
     },
-    set: (key, value) => {
+    set(key, value) {
       try {
         localStorage.setItem(
           [options.prefix, key].join('/'),
@@ -21,7 +21,7 @@ const createCache = (options: TCacheOptions) => {
         return false;
       }
     },
-    unset: (key) => {
+    unset(key) {
       localStorage.removeItem([options.prefix, key].join('/'));
     },
   };
