72cf417f9464cb6c437ff747f209887339c0cf03
tdeekens <tobias.deekens@commercetools.com>
2022-10-10 21:10:28 +0200

    refactor: to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
-/** @type {import('vite').UserConfig} */
-
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
