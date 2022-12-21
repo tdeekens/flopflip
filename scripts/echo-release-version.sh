47daeb4a48432543ab9ff538063578ff18d580bf
tdeekens <tobias.deekens@commercetools.com>
2022-12-21 17:27:14 +0100

    fix: improve logging

--- scripts/echo-release-version.sh
+++ scripts/echo-release-version.sh
@@ -2,11 +2,25 @@
 
 set -e
 
-# Running "changeset version" to know the new release version
+echo "Running 'changeset version' to know the new release version"
+
 yarn changeset version &>/dev/null
 
+echo "Running 'git status' to see the worktree changes"
+
+git status
+
+echo "Determining the version from the package.json of a package"
 release_version=$(node -e "console.log(require('./packages/react/package.json').version)")
+
+echo "Version for release is $release_version"
+
+echo "Running 'git reset' and exporting to GITHUB_OUTPUT"
 
 git reset --hard &>/dev/null
 
-echo "$release_version"
+echo "VERSION=$release_version" >> $GITHUB_OUTPUT
+
+echo "GITHUB_OUTPUT is:"
+echo $GITHUB_OUTPUT
+
