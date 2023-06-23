73a0a063a4a1bfa1514cd66735c62511a16a3b22
tdeekens <tobias.deekens@commercetools.com>
2023-06-23 11:33:52 +0200

    chore: downgrade

--- packages/splitio-adapter/src/adapter/adapter.ts
+++ packages/splitio-adapter/src/adapter/adapter.ts
@@ -34,8 +34,8 @@ type TSplitIOAdapterState = {
   treatmentAttributes?: SplitIO.Attributes;
 };
 type SplitIOClient = {
-  client: SplitIO.IAsyncClient;
-  manager: SplitIO.IAsyncManager;
+  client: SplitIO.IClient;
+  manager: SplitIO.IManager;
 };
 
 const normalizeFlag = (
