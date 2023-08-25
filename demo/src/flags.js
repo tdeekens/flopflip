0909a2f58a003157b3e212b37a6bf50bba6feafb
Tobias Deekens <nerd@tdeekens.name>
2023-08-25 16:29:38 +0200

    refactor: to migrate to pnpm (#1805)
    * refactor: to migrate to pnpm
    
    * fix: remove constraints check
    
    * fix: post install
    
    * fix: run script
    
    * chore: add changeset
    
    * chore: removal dangling yarn usages

--- demo/src/flags.js
+++
@@ -1,9 +0,0 @@
-export const INCREMENT_ASYNC_BUTTON = 'incrementAsync';
-export const DECREMENT_ASYNC_BUTTON = 'decrementAsync';
-export const INCREMENT_SYNC_BUTTON = 'incrementSync';
-
-export default {
-  [INCREMENT_ASYNC_BUTTON]: false,
-  [DECREMENT_ASYNC_BUTTON]: true,
-  [INCREMENT_SYNC_BUTTON]: 'yellow',
-};
