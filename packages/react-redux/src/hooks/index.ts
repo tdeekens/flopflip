ae8651723aad87baee87358eee0a664869654125
Carlos Cortizas <carlos.martines@commercetools.com>
2024-02-27 15:21:07 +0100

    refactor(react-redux): export use-all-feature-toggles hook

--- packages/react-redux/src/hooks/index.ts
+++ packages/react-redux/src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { default as useAdapterReconfiguration } from './use-adapter-reconfiguration';
 export { default as useAdapterStatus } from './use-adapter-status';
+export { default as useAllFeatureToggles } from './use-all-feature-toggles';
 export { default as useFeatureToggle } from './use-feature-toggle';
 export { default as useFeatureToggles } from './use-feature-toggles';
 export { default as useFlagVariation } from './use-flag-variation';
