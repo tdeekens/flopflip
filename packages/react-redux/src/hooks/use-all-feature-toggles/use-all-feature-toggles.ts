851e304801e0e62c514bfb51ab44aadf79b72cdd
Carlos Cortizas <carlos.martines@commercetools.com>
2024-02-27 13:21:14 +0100

    refactor(react-redux): remove adapters parameter

--- packages/react-redux/src/hooks/use-all-feature-toggles/use-all-feature-toggles.ts
+++ packages/react-redux/src/hooks/use-all-feature-toggles/use-all-feature-toggles.ts
@@ -1,20 +1,18 @@
-import { type TAdapterIdentifiers, type TFlags } from '@flopflip/types';
+import { useAdapterContext } from '@flopflip/react';
+import type { TFlags } from '@flopflip/types';
 import { useSelector } from 'react-redux';
 
 import { selectFlags } from '../../ducks/flags';
 
-export default function useAllFeatureToggles(
-  adapterIdentifiers: TAdapterIdentifiers
-): TFlags {
+export default function useAllFeatureToggles(): TFlags {
+  const adapterContext = useAdapterContext();
   const allFlags = useSelector(selectFlags());
 
-  let allFlagsFromAdapterIdentifers: TFlags = {};
-  for (const adapterIdentifier of adapterIdentifiers) {
-    allFlagsFromAdapterIdentifers = {
-      ...allFlagsFromAdapterIdentifers,
+  return adapterContext.adapterEffectIdentifiers.reverse().reduce<TFlags>(
+    (_allFlags, adapterIdentifier) => ({
+      ..._allFlags,
       ...allFlags[adapterIdentifier],
-    };
-  }
-
-  return allFlagsFromAdapterIdentifers;
+    }),
+    {}
+  );
 }
