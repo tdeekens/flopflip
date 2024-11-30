ac8c4caf918cc885e975a7987250eba01bbfd3b3
tdeekens <tobias.deekens@commercetools.com>
2024-11-30 14:11:51 +0100

    refactor: remove jest configs

---
+++ packages/adapter-utilities/src/denormalize-flag-name/denormalize-flag-name.spec.ts
@@ -0,0 +1,11 @@
+import { TFlagName } from '@flopflip/types';
+import denormalizeFlagName from './denormalize-flag-name';
+
+describe('with camel case', () => {
+  it('should camel case to uppercased flag names', () => {
+    const input: TFlagName = 'aFlag';
+    const expected: string = 'a-flag';
+    
+    expect(denormalizeFlagName(input)).toEqual(expected);
+  });
+});
