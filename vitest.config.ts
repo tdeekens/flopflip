78a9837cc98ce9743c6af52e0ea1ab7176fd516c
tdeekens <tobias.deekens@commercetools.com>
2024-11-30 14:21:25 +0100

    refactor: to setup vitest with jsdom

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup-tests.ts',
+  },
+});
