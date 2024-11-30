27ef43effd22d1e71a9850819604b6cd26cfb448
tdeekens <tobias.deekens@commercetools.com>
2024-11-30 15:05:41 +0100

    test: fix react testing setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
