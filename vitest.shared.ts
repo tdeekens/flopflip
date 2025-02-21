fb4d37117dd2fd5e7a22ba366b7e3d7654088d3c
Tobias Deekens <nerd@tdeekens.name>
2025-02-21 17:21:55 +0100

    test: to use automatic runtime finally (#2003)

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'classic',
+      jsxRuntime: 'automatic',
     }),
   ],
   test: {
